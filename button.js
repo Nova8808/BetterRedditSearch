50cd414ca7e7cda967f06803d16a2082d6886413
Steve S <nova8808@yahoo.com>
2022-09-18 12:08:47 -0600

    v.2: go directly to subreddit, instantly search without needing to press enter

--- button.js
+++ button.js
@@ -10,14 +10,21 @@ searchButton.addEventListener('click', () => {
 
 function searchFunction(whatToSearch, incog, subR){
     var search = '';
-    if (subR !== ''){
-        search = ('/r/' + subR + ' ' + whatToSearch);
-    } else search = (' ' + whatToSearch);
+
+    if (whatToSearch == '' && subR != ''){
+        search = "http://www.reddit.com/r/"+subR;
+    }
+    else if (subR !== ''){
+        search = "https://www.google.com/search?q=site%3Areddit.com%2Fr%2F"+subR+"+"+whatToSearch+"&rlz=1C1CHBF_enUS856US856&oq=site%3Areddit.com%2Fr%2F"+subR+"+"+whatToSearch+"+&aqs=chrome..69i57j69i58.279285j1j4&sourceid=chrome&ie=UTF-8"
+        // search = ('/r/' + subR + ' ' + whatToSearch);
+    } else search = "https://www.google.com/search?q=site%3Areddit.com+" + whatToSearch + "&rlz=1C1CHBF_enUS856US856&oq=site%3Areddit.com+" + whatToSearch + "&aqs=chrome.0.69i59j69i58.768017j1j4&sourceid=chrome&ie=UTF-8";
+    //search = (' ' + whatToSearch);
 
     if (incog == true){
-    chrome.windows.create({url: "site:reddit.com" + search, incognito: true});
+    // chrome.windows.create({url: "site:reddit.com" + search, incognito: true});
+    chrome.windows.create({url: search, incognito: true});
     } else {
-        chrome.tabs.create({ url: 'site:reddit.com' + search});
+        chrome.tabs.create({ url: search});
     }
 
 }
